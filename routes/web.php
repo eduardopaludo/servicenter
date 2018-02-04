3c839dbc1d80b2ba79a0aaef3c36d0374f75d0a3
Eduardo Paludo <eduardopaludo@Eduardos-MacBook-Pro.local>
2018-02-04 18:15:00 -0200

    Liberado uso do Eloquent

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 $app->get('/', function () use ($app) {
     return $app->version();
 });
+
+$app->group(['prefix' => 'api/v1', 'namespace' => 'Api\V1'], function () use ($app) {
+    $app->get('partners', 'PartnersController@index');
+//    $app->get('restaurants/{id:[0-9]+}', 'RestaurantsController@show');
+//    $app->post('restaurants', 'RestaurantsController@store');
+//    $app->put('restaurants/{id:[0-9]+}', 'RestaurantsController@update');
+//    $app->post('restaurants/{id:[0-9]+}', 'RestaurantsController@update');
+//    $app->delete('restaurants/{id:[0-9]+}', 'RestaurantsController@destroy');
+//
+//
+//    $app->post('restaurants/{id:[0-9]+}/address', 'RestaurantsController@address');
+//    $app->post('restaurants/{id:[0-9]+}/upload', 'RestaurantsController@upload');
+//
+//    $app->get('restaurants/{id:[0-9]+}/photos', 'RestaurantPhotosController@index');
+//    $app->post('restaurants/photos', 'RestaurantPhotosController@store');
+//    $app->delete('restaurants/photos/{id:[0-9]+}', 'RestaurantPhotosController@destroy');
+//
+//    $app->get('auth/me', 'AuthController@me');
+//    $app->post('auth/change-password', 'AuthController@changePassword');
+//    $app->post('auth/edit-profile', 'AuthController@editProfile');
+//    $app->get('auth/logout', 'AuthController@logout');
+});
