6e20a8192469c3de76ec55d5c4dd66c8461ecb32
Eduardo Paludo <eduardopaludo@Eduardos-MacBook-Pro.local>
2018-02-04 17:59:57 -0200

    Criado PartnersController

---
+++ app/Http/Controllers/Api/V1/PartnersController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eduardopaludo
+ * Date: 2/4/18
+ * Time: 5:55 PM
+ */
+
+namespace App\Http\Controllers\Api\V1;
+
+
+use Laravel\Lumen\Routing\Controller;
+
+class PartnersController extends Controller
+{
+
+}
