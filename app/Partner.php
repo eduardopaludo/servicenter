d048ac99aa2b566b56001761836f753de01a1022
Eduardo Paludo <eduardopaludo@Eduardos-MacBook-Pro.local>
2018-02-04 17:53:49 -0200

    Criado model Partner

---
+++ app/Partner.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: eduardopaludo
+ * Date: 2/4/18
+ * Time: 5:52 PM
+ */
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Partner extends Model
+{
+    protected $fillable = ['user_id', 'description'];
+}
